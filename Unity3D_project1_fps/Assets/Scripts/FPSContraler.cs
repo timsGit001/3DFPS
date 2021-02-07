7616d88959467271f8f4b0e07918c199d0f38c26
timsGit001 <75553502+timsGit001@users.noreply.github.com>
2021-02-07 17:01:35 +0800

    添加人物簡易移動

---
+++ Unity3D_project1_fps/Assets/Scripts/FPSContraler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class FPSContraler : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 2000)]
+    public float speed;
+    [Header("轉動速度"), Range(0, 2000)]
+    public float turnSpeed;
+
+    private Animator ani;
+    private Rigidbody rig;
+
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody>();
+    }
+
+    private void Move()
+    {
+        // 取 前後值
+        float v = Input.GetAxis("Vertical");
+        // 取 左右值
+        float h = Input.GetAxis("Horizontal");
+
+        // 前後移動
+        rig.MovePosition(transform.position + transform.forward * v * speed * Time.deltaTime + transform.right * h * speed * Time.deltaTime);
+
+        // 移動動畫
+        ani.SetBool("runSwitch", (v != 0 || h != 0));
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+}
