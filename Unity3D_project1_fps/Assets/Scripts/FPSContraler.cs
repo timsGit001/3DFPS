46bff4b85850a96ee395afc85449a9c9bf28fde3
timsGit001 <75553502+timsGit001@users.noreply.github.com>
2021-02-21 13:06:39 +0800

    子彈發射&架設針對自己的Camera&跳躍

--- Unity3D_project1_fps/Assets/Scripts/FPSContraler.cs
+++ Unity3D_project1_fps/Assets/Scripts/FPSContraler.cs
@@ -2,20 +2,54 @@
 
 public class FPSContraler : MonoBehaviour
 {
-    [Header("移動速度"), Range(0, 2000)]
+    #region 基本欄位
+    [Header("移動速度"), Range(0, 200)]
     public float speed;
-    [Header("轉動速度"), Range(0, 2000)]
+    [Header("轉動速度"), Range(0, 1000)]
     public float turnSpeed;
+    [Header("跳躍力道"), Range(0, 1000)]
+    public float jumpForce;
+    [Header("地板偵測")]
+    public Vector3 floorOffset;
+    [Header("地板偵測半徑"), Range(0, 10)]
+    public float floorRadius = 1;
 
     private Animator ani;
     private Rigidbody rig;
+    #endregion
+
+    #region 開槍欄位
+    [Header("子彈")]
+    public GameObject bullet;
+    [Header("子彈生成點")]
+    public Transform bulletSpawn;
+    [Header("子彈目前數量")]
+    public int bulletCurrent;
+    [Header("子彈總數")]
+    public int bulletTotal;
+    [Header("子彈速度")]
+    public int bulletSpeed;
+    #endregion
 
     private void Awake()
     {
         ani = GetComponent<Animator>();
         rig = GetComponent<Rigidbody>();
+
+        // 隱藏鼠標
+        Cursor.visible = false;
     }
 
+    private void OnDrawGizmos()
+    {
+        // 畫出 物理地板偵測範圍 (debug用 不會顯示在Game畫面)
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawSphere(transform.position + floorOffset, floorRadius);
+    }
+
+    /// <summary>
+    /// 移動
+    /// </summary>
     private void Move()
     {
         // 取 前後值
@@ -28,10 +62,46 @@ public class FPSContraler : MonoBehaviour
 
         // 移動動畫
         ani.SetBool("runSwitch", (v != 0 || h != 0));
+
+        // 取左右的值
+        float x = Input.GetAxis("Mouse X");
+        transform.Rotate(0, x * turnSpeed * Time.deltaTime, 0);
+        
     }
+
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+        // 取 特定球體的碰撞結果(只對Layer8偵測)
+        Collider[] hits = Physics.OverlapSphere(transform.position + floorOffset, floorRadius, 1<<8);
+
+        if (hits.Length > 0 && hits[0] && Input.GetKeyDown(KeyCode.Space)) {
+            rig.AddForce(0, jumpForce, 0);
+        }
+    }
+
+    /// <summary>
+    /// 開槍
+    /// </summary>
+    private void Fire()
+    {
+        if ( Input.GetKeyDown(KeyCode.Mouse0) )
+        {
+            // 生成子彈
+            GameObject tempBullet = Instantiate<GameObject>(bullet, bulletSpawn.position, bulletSpawn.rotation);
+
+            // 子彈飛出
+            tempBullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.up * bulletSpeed);
+        }
+    }
+
 
     private void Update()
     {
         Move();
+        Jump();
+        Fire();
     }
 }
