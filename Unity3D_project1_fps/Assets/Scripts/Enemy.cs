8150071d4068c09657aeac5b64cfa9e50dcaf4c6
timsGit001 <75553502+timsGit001@users.noreply.github.com>
2021-03-07 13:01:44 +0800

    敵人追蹤&開槍

--- Unity3D_project1_fps/Assets/Scripts/Enemy.cs
+++ Unity3D_project1_fps/Assets/Scripts/Enemy.cs
@@ -14,12 +14,19 @@ public class Enemy : MonoBehaviour
     private NavMeshAgent nav;
 
     private Animator ani;
-    private Rigidbody rig;
 
     [Header("移動速度"), Range(0f, 30f)]
     public float speed = 2.5f;
     [Header("攻擊範圍"), Range(2f, 100f)]
     public float atkRange = 10f;
+    [Header("子彈")]
+    public GameObject bullet;
+    [Header("子彈生成點")]
+    public Transform bulletSpawn;
+    [Header("子彈速度"), Range(0f, 5000f)]
+    public float bulletSpeed;
+    [Header("開槍間隔"), Range(0f, 10f)]
+    public float cdTime;
 
     // Start is called before the first frame update
     void Awake()
@@ -31,7 +38,6 @@ public class Enemy : MonoBehaviour
         nav.stoppingDistance = atkRange;
 
         ani = GetComponent<Animator>();
-        rig = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -42,14 +48,34 @@ public class Enemy : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = new Color(1,0,0,0.3f);
+        Gizmos.color = new Color(1, 0, 0, 0.3f);
         Gizmos.DrawSphere(transform.position, atkRange);
     }
 
     void Tracking()
     {
         nav.SetDestination(player.position);
+
+        bool preparingAtk = nav.remainingDistance <= atkRange;
         // 移動動畫
-        ani.SetBool("runSwitch", rig.velocity.magnitude > 0);
+        ani.SetBool("runSwitch", !preparingAtk);
+        Fire(preparingAtk);
+    }
+
+    void Fire(bool preparingAtk)
+    {
+        if (preparingAtk)
+        {
+            // 開槍動畫
+            ani.SetTrigger("doShoot");
+            // 生成子彈
+            GameObject tempBullet = Instantiate<GameObject>(bullet, bulletSpawn.position, bulletSpawn.rotation);
+
+            // 子彈飛出
+            tempBullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.up * bulletSpeed);
+
+            // 開槍聲響
+            // aud.PlayOneShot(fireFsx, Random.Range(0.8f, 1.1f));
+        }
     }
 }
